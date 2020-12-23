816694bdeba8b0bf83a4c6ed4475a31f7c493e04
subhashjakhar <76390128+subhashjakhar@users.noreply.github.com>
2020-12-23 17:11:17 +0530

    as
    as

---
+++ TestDevOps23/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestDevOps23.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
